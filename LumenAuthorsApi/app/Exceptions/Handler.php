8029489a7bccec52df1792917e872cb5bee86048
Siddharth Mehta <mehtasiddharth1991@gmail.com>
2021-03-14 23:03:33 -0700

    Lumen

---
+++ LumenAuthorsApi/app/Exceptions/Handler.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use App\Traits\ApiResponser;
+use Illuminate\Auth\AuthenticationException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    use ApiResponser;
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof HttpException){
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+            return $this->errorResponse($message, $code);
+        } else if($exception instanceof ModelNotFoundException){
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Instance of ${model} does not exist with the given id", Response::HTTP_NOT_FOUND);
+        } else if($exception instanceof AuthorizationException){
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
+        } else if($exception instanceof AuthenticationException){
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
+        } else if($exception instanceof ValidationException){
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        if(env('APP_DEBUG', false)){
+            return parent::render($request, $exception);
+        }
+        return $this->errorResponse('Unexpected Error. Try Again Later.', Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
